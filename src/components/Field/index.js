27f752fa1d654caaf994f0a2c701b712103073d5
Abdoula Voraev <abdoulavoraev@Abdoulas-MacBook-Pro.local>
2021-09-04 16:11:12 +0200

    added obstacle, collision and falling animation

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import garbage from 'src/assets/images/garbage.png';
 
 import './styles.scss';
 
 const Field = () => (
   <div className="field">
-    <div className="obstacle" />
+    <div className="obstacle" style={{ backgroundImage: `url(${garbage})` }} />
     {/* <div className="obstacle2" /> */}
   </div>
 );
--- src/components/Robot/index.js
+++ src/components/Robot/index.js
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.scss';
+import runRobot from 'src/assets/images/run_robot.gif';
+import fallRobot from 'src/assets/images/robot_fall_px.gif';
 
 const Robot = () => {
   let jumping = false;
+  let lost = false;
+  let nearlyDown = true; // instead of checking vertical position at some intervals
+  // we are going to let game know when character nearly reached the floor, in which
+  // case : nearlyDown will be true.
+  
+  
 
   // Keyboard input with customisable repeat (set to 0 for no key repeat)
   //
@@ -44,13 +52,16 @@ const Robot = () => {
     };
   };
 
-  KeyboardController({
-    37: function() { goLeft()},
-    38: function() { goJump()},
-    39: function() { goRight()},
-    32: function() { goJump()},
-  }, 50);
-  // 200 originally, (works best with 50)
+  KeyboardController(
+    {
+      37: function() { goLeft()},
+      38: function() { goJump()},
+      39: function() { goRight()},
+      32: function() { goJump()},
+    },
+    75,
+  );
+  // 200 originally, (works best with 50 or 100)
 
   const goJump = () => {
     const robot = document.querySelector('.robot');
@@ -60,12 +71,43 @@ const Robot = () => {
     else {
       robot.style.animationName = 'jump';
       jumping = true;
-
       setTimeout(() => {
-        robot.style.animationName = 'none';
+        nearlyDown = false; // player jumped so nearlydown is false
+      }, 100);
+      setTimeout(() => {
+        nearlyDown = true; // now player nearly reached the floor
+      }, 700);
+      setTimeout(() => {
+        robot.style.animationName = '';
         jumping = false;
-      }, 950);
+      }, 750);
     }
+  };
+
+  let robotElement;
+  let obstacleElement;
+  const isCollide = () => {
+    // for (let i = 1; i <= 100 && lost === false; i += 1) {
+    const intervalID = setInterval(() => {
+      const robotPosition = robotElement.getBoundingClientRect();
+      const obstaclePosition = obstacleElement.getBoundingClientRect();
+      const differenceRight = robotPosition.right - obstaclePosition.left;
+      const differenceLeft = robotPosition.left - obstaclePosition.right;
+      // const differenceTop = robotPosition.bottom - obstaclePosition.top;
+      console.log(differenceLeft);
+      if (differenceRight > 35 && differenceLeft < -5 && nearlyDown) { //(differenceTop > -20)
+        robotElement.querySelector('.robot_image').src = fallRobot;
+        console.log("Touché à gauche");
+        lost = true;
+        clearInterval(intervalID);
+      }
+      // if (differenceLeft < -10) {
+      //   console.log("Touché à droite")
+      // }
+      // if (lost === true) {
+      //   clearInterval(intervalID);
+      // }
+    }, 100);
   };
 
   const goRight = () => {
@@ -75,9 +117,10 @@ const Robot = () => {
     const leftPx = robot.style.left;
     const left = leftPx.split('px')[0];
 
-    if (screenWidth - left > 30) {
+    if (screenWidth - left > 130) {
       const newLeft = `${parseInt(left, 10) + 30}px`;
       robot.style.left = newLeft;
+      // setLeft(left + 30);
     }
   };
 
@@ -89,51 +132,63 @@ const Robot = () => {
     if (left >= 0) {
       const newLeft = `${parseInt(left, 10) - 30}px`;
       robot.style.left = newLeft;
+      // setLeft(left - 30);
     }
+    // isCollide();
   };
 
-  const runningAnimation = () => {
-    const robot = document.querySelector('.robot');
-    for (let i = 1; i <= 14; i += 1) {
-      setTimeout(() => {
-        robot.className = `robot run${i}`;
-        if (i === 14) {
-          runningAnimation();
-        }
-      }, 50 * (i)); // picture changes every 50ms
-    }
-  };
 
-  const detectCollision = () => {
-    const robotElement = document.querySelector('.robot');
-    const obstacleElement = document.querySelector('.obstacle');
 
-    for (let i = 1; i <= 30000; i += 1) {
-      setTimeout(() => {
-        const robotPosition = robotElement.getBoundingClientRect();
-        const obstaclePosition = obstacleElement.getBoundingClientRect();
-        const difference = robotPosition.right - obstaclePosition.left;
-        console.log(difference);
 
-        if (i === 14) {
-          detectCollision();
-        }
-      }, 50 * (i)); // picture changes every 50ms
-    }
-  }
+
+
+
+
+
+  // const [difference, setdifference] = useState(0);
+
+  // const runningAnimation = () => {
+  //   const robotElement = document.querySelector('.robot');
+  //   const obstacleElement = document.querySelector('.obstacle');
+  //   for (let i = 1; i <= 14 && lost === false; i += 1) {
+  //     setTimeout(() => {
+  //       robotElement.className = `robot run${i}`; // running animation consists on this line
+
+  //       // const robotPosition = robotElement.getBoundingClientRect();
+  //       // const obstaclePosition = obstacleElement.getBoundingClientRect();
+  //       // const differenceRight = robotPosition.right - obstaclePosition.left;
+  //       // const differenceLeft = robotPosition.right - obstaclePosition.left;
+  //       // // difference = differenceRight;
+  //       // // setdifference(differenceRight)
+  //       // // const differenceLeft
+  //       // // console.log(differenceRight);
+
+  //       // if (differenceRight > 0) {
+  //       //   console.log("Perdu")
+  //       //   lost = true;
+  //       // }
+
+  //       // if (i === 14 && lost === false) {
+  //       //   runningAnimation();
+  //       // }
+  //       // console.log("animation", left);
+  //     }, 50 * (i)); // picture changes every 50ms
+  //   }
+  // };
 
   useEffect(() => {
-    runningAnimation();
-    detectCollision();
+    robotElement = document.querySelector('.robot');
+    obstacleElement = document.querySelector('.obstacle');
+    isCollide();
   }, []);
 
-
-
   return (
-    <div className="robot" style={{ left: '50px'}}>
-      {/* <img src={imgSrc} /> */}
-    </div>
-    // backgroundImage: `url(${imgSrc})`
+    <>
+      <div className="robot" style={{ left: "50px" }}> {/* style={{ left: `${left}px` }} */}
+        <img src={runRobot} className="robot_image" />
+      </div>
+      {/* <p className="difference">{left}</p> */}
+    </>
   );
 };
 
